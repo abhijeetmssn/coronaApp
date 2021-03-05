00014b6bd10b2b19364e8c691f5d3d96c8792355
abhijeetmssn.com <abhijeetmssn924@gmail.com>
2021-03-05 21:28:36 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-class CovidData {
+class CovidDataLog {
     
     constructor() {
         this.id;
@@ -7,57 +7,72 @@ class CovidData {
         this.total_doses_administered;
         this.total_doses_in_fully_vaccinated_individuals;
         this.total_individuals_fully_vaccinated;
+
+        this.loadCovidInformationToLocalStorage();
     }
 
-    loadCovidData = () => {
+    loadCovidInformationToLocalStorage = () => {
             $.ajax({
                 type: 'GET',
                 url: '../vaccine_doses.json',
                 data: { get_param: 'value' },
                 dataType: 'json',
                 success: function (data) {
-                    $.each(data, function (index, element) {
-                        data.forEach((element, index) => {
-                            let objToStore = {
-                                ...element,
+                    $.each(data, () =>  {
+                        data.forEach((covidDataObj, index) => {
+                            let dataRetrieved = {
+                                ...covidDataObj,
                                 id: index + 1
                             }
-                            localStorage.setItem(element.report_date, JSON.stringify(objToStore));
+                            localStorage.setItem(covidDataObj.report_date, JSON.stringify(dataRetrieved));
                         });
                     });
                 }
             });
     }
+}
 
-    displayUnorderedList = () => {
+$(() => {
+    $('#loadButton').click(function () {
+        let coronaData = new CovidDataLog();
+    })
+})
+
+$(() => {
+    $('#displayButton').click(function () {
         for (var i = 0; i < localStorage.length; i++) {
             $('ul').append(`<li><span onClick="displayIndividualRecord(${i})">${localStorage.key(i)}</span></li><br>`);
         }
         if (localStorage.length != 0)
         {
         $("#displayButton").prop('disabled', true); 
+        }else{
+            window.alert("Data is not loaded in local storage");
         }
-    }
-
-}
-
-let cData = new CovidData();
-$(() => {$('#loadButton').click(function () {
-cData.loadCovidData();
-})
-})
-
-$(() => {
-    $('#displayButton').click(function () {
-        cData.displayUnorderedList();
     });
 });
 
-const displayIndividualRecord = (index) => {
-    const fetchData = JSON.parse(localStorage.getItem(localStorage.key(index)));
-    document.getElementById("previous_day_doses_administered").innerHTML =" Previous day Doses Administered: " + fetchData.previous_day_doses_administered;
-    document.getElementById("total_doses_administered").innerHTML =  "Total Doses Administered: " + fetchData.total_doses_administered;
-    document.getElementById("total_doses_in_fully_vaccinated_individuals").innerHTML ="Total Doses in Fully Vaccinated Individuals: " + fetchData.total_doses_in_fully_vaccinated_individuals;
-    document.getElementById("total_individuals_fully_vaccinated").innerHTML = "Total Individuals Fullu Vaccinated: " + fetchData.total_individuals_fully_vaccinated;
+const displayIndividualRecord = (id) => {
+    const fetchedCovidData = JSON.parse(localStorage.getItem(localStorage.key(id)));
+    let previousDoses = document.getElementById("previous_day_doses_administered");
+    previousDoses.innerHTML = " Previous day Doses Administered: " + fetchedCovidData.previous_day_doses_administered;
+    
+
+    let totalDoses = document.getElementById("total_doses_administered");
+    totalDoses.innerHTML = " Total Doses Administered: " + fetchedCovidData.previous_day_doses_administered;
+
+    
+
+    let vaccinatedDoses = document.getElementById("total_doses_in_fully_vaccinated_individuals");
+    vaccinatedDoses.innerHTML = "Total Doses in Fully Vaccinated Individuals: " + fetchedCovidData.previous_day_doses_administered;
+   
+
+   let fullyDoses = document.getElementById("total_individuals_fully_vaccinated");
+   fullyDoses.innerHTML = "Total Individuals Fully Vaccinated: " + fetchedCovidData.previous_day_doses_administered;
+    
 }
+
+
+
+
 
